38c1f0dbb5e7927a19b147a815bc99ba51014787
James Wilson <wilsjame@oregonstate.edu>
2018-05-04 18:28:39 -0700

    bind port with listening socket

--- chatserve.py
+++ chatserve.py
@@ -3,14 +3,26 @@
 #** Date: 5/4/2018
 #** Description: chatserve.py 
 #*********************************************************************/
-from socket import *
+
+# access socket methods without having to prepend 'socket.'
+from socket import * 
 
 def main():
 
 	# hard code server's handle
-	# get port # from command line
+	handle = 'Host A (server)'
+	print(handle)
+
+	# get port # from command line and convert to int
+	serverPort = input('Enter server port #: ')
+	print('You entered ' + serverPort)
+
 	# create IPv6 TCP listening socket
+	serverSocket = socket(AF_INET6, SOCK_STREAM)
+
 	# bind port # with listening socket
+	serverSocket.bind(('', int(serverPort)))
+
 	# start listening
 	# wait, indefinitely, for client to come knocking
 	# give incoming client their own connection socket
