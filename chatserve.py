348cd8dc9f0f7973fa58706ded72ace99db8b5aa
James Wilson <wilsjame@oregonstate.edu>
2018-05-04 18:04:23 -0700

    outline program logic

---
+++ chatserve.py
@@ -0,0 +1,30 @@
+#/*********************************************************************
+#** Author: James G Wilson
+#** Date: 5/4/2018
+#** Description: chatserve.py 
+#*********************************************************************/
+from socket import *
+
+def main():
+
+	# hard code server's handle
+	# get port # from command line
+	# create IPv6 TCP listening socket
+	# bind port # with listening socket
+	# start listening
+	# wait, indefinitely, for client to come knocking
+	# give incoming client their own connection socket
+	# chat!
+	#	prepend handle
+	#	respond to host B
+	#	alternate
+	#	close connection with command "\quit"
+	# close connection socket
+	# repeat 
+	# signal handler: catch SIGINT to shutdown server
+
+if __name__ == "__main__":
+	# execute only if run as a script
+	main()
+
+
